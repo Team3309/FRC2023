30eba676f72bceb463ff534d95000c6b71b4f30b
MarkGhebrial <mghebrialjr@icloud.com>
2023-01-14 15:04:51 -0800

    Removed FollowTrajectory and Photon Camera

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,7 +3,9 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
-
+/*This is Zach and Aidan
+ * A wild "3309" has appeared
+ */
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -15,5 +17,7 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+  
   }
+
 }
