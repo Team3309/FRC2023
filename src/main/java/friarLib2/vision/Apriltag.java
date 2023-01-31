86e04a8d5eba885db93d5386a1357eefbab0067a
RoboChicken46 <aidan.mirand@servitehs.org>
2023-01-30 20:28:07 -0800

    Started AprilTag code

--- src/main/java/friarLib2/vision/Apriltag.java
+++ src/main/java/friarLib2/vision/Apriltag.java
@@ -1,9 +1,27 @@
 package friarLib2.vision;
 
+//Imports for PhotonVision
+import org.photonvision.PhotonCamera;
+import org.photonvision.common.*;
+import org.photonvision.common.dataflow.*;
+import org.photonvision.common.dataflow.structures.*;
+import org.photonvision.common.hardware.*;
+import org.photonvision.common.networktables.*;
+import org.photonvision.targeting.*;
+
+//WPILib imports for AprilTag
 import edu.wpi.first.apriltag.AprilTag;
+import edu.wpi.first.apriltag.AprilTagDetection;
+import edu.wpi.first.apriltag.AprilTagDetector;
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.apriltag.AprilTagPoseEstimate;
+import edu.wpi.first.apriltag.AprilTagPoseEstimator;
 import edu.wpi.first.apriltag.jni.AprilTagJNI;
 
-public class Apriltag{
-    
+
+public class Apriltag { 
+    PhotonCamera camera = new PhotonCamera("photonvision"); 
+    var result = camera.getLatestResult();
     
 }
