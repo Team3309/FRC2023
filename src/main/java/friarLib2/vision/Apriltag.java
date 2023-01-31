4ef2be645e64e101241f5bd6385953815cf04d77
RoboChicken46 <aidan.mirand@servitehs.org>
2023-01-30 20:59:44 -0800

    Added vision code

--- src/main/java/friarLib2/vision/Apriltag.java
+++ src/main/java/friarLib2/vision/Apriltag.java
@@ -1,5 +1,7 @@
 package friarLib2.vision;
 
+import javax.naming.spi.DirStateFactory.Result;
+
 //Imports for PhotonVision
 import org.photonvision.PhotonCamera;
 import org.photonvision.common.*;
@@ -8,6 +10,7 @@ import org.photonvision.common.dataflow.structures.*;
 import org.photonvision.common.hardware.*;
 import org.photonvision.common.networktables.*;
 import org.photonvision.targeting.*;
+import org.photonvision.PhotonUtils;
 
 //WPILib imports for AprilTag
 import edu.wpi.first.apriltag.AprilTag;
@@ -18,10 +21,18 @@ import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.apriltag.AprilTagPoseEstimate;
 import edu.wpi.first.apriltag.AprilTagPoseEstimator;
 import edu.wpi.first.apriltag.jni.AprilTagJNI;
+import edu.wpi.first.cscore.CameraServerJNI;
 
 
 public class Apriltag { 
-    PhotonCamera camera = new PhotonCamera("photonvision"); 
-    var result = camera.getLatestResult();
+    static PhotonCamera camera = new PhotonCamera("photonvision"); 
+    public static void main(String[]args) {
+        var result = camera.getLatestResult();
+    }
+    boolean hasTargets = result.hasTargets();
+    
+
+
+        
     
 }
