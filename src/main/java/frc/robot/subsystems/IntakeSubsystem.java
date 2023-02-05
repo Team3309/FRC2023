55615ff4cb0e8178a254d7036cc68d5ba59e533e
RoboChicken46 <aidan.mirand@servitehs.org>
2023-02-04 19:34:16 -0800

    added IntakeSubsytem

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,31 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+import static frc.robot.Constants.Intake.*;
+
+
+public class IntakeSubsystem extends SubsystemBase {
+    private final WPI_TalonSRX leftIntakeMotor;
+    private final WPI_TalonSRX rightIntakeMotor;
+    
+}
+public IntakeSubsystem() {
+    leftIntakeMotor = new WPI_TalonSRX(LEFT_INTAKE_MOTOR_ID);
+    rightIntakeMotor = new WPI_TalonSRX(RIGHT_INTAKE_MOTOR_ID);
+
+    leftIntakeMotor.setNeutralMode(NeutralMode.Brake);
+    rightIntakeMotor.setNeutralMode(NeutralMode.Brake);
+
+    leftIntakeMotor.setInverted(true);
+    rightIntakeMotor.setInverted(false);
+
+    
+
+}
