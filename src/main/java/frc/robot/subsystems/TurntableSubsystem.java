967dc6659ed0b12198479443de31c7d8e10870b9
RoboChicken46 <aidan.mirand@servitehs.org>
2023-02-08 00:22:25 -0800

    Updated and mostly finished the turntableSubsystem

--- src/main/java/frc/robot/subsystems/TurntableSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurntableSubsystem.java
@@ -1,15 +1,15 @@
 package frc.robot.subsystems;
-
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.Constants.Turntable;
+
+import static frc.robot.Constants.Turntable.*;
 
 public class TurntableSubsystem extends SubsystemBase {
-    private static final double TURNTABLE_MOTOR_POWER = 0;
     private final WPI_TalonFX turntableMotor;
 
     public TurntableSubsystem() {
@@ -18,20 +18,59 @@ public class TurntableSubsystem extends SubsystemBase {
         turntableMotor.setNeutralMode(NeutralMode.Brake);
     }
     
+    public int check;
+
     /*
      * Start the Turntable at the motor power
      */
-    public void starTurntable() {
+    public void startTurntable() {
         turntableMotor.set(ControlMode.PercentOutput, TURNTABLE_MOTOR_POWER);
     }
 
     /*
-     * Turn off the conveyer
+     * Turn off the Turntable
      */
     public void stopTurntable() {
         turntableMotor.stopMotor();
     }
+    
+    /*
+     * Set default position
+     */
+    public void defaultPosition() {
+        turntableMotor.set(ControlMode.Position, TURNATABLE_DEFAULT_POSITION);
+            int check = 0;
+    }
 
-    
-    
+    /*
+     * Turn to orient the cone
+     */
+    public void orientCone() {
+        turntableMotor.set(ControlMode.Position, TURNTABLE_ORIENTATION);
+            int check = 1;
+    }
+
+    /**
+     * @return If the turntable is Default position
+     */
+    public boolean isDefaultPosition() {
+        boolean isDefault = check == 0;
+
+        return isDefault;
+    }
+
+    /**
+     * @return If the turntable is at a oriented position
+     */
+    public boolean isOrientedPosition() {
+        boolean isOriented = check == 1;
+
+        return isOriented;
+    }
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putBoolean("Is turntable default", isDefaultPosition());
+        SmartDashboard.putBoolean("Is turntable oriented", isOrientedPosition());
+    }
 }
