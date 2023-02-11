7364a65c4e9b7f7b4b4f7a1357eec86bb152f4d1
RoboChicken46 <aidan.mirand@servitehs.org>
2023-02-11 14:18:45 -0800

    fixed indentation

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -15,37 +15,37 @@ public class IntakeSubsystem extends SubsystemBase {
     private final WPI_TalonSRX topIntakeMotor;
     private final WPI_TalonSRX bottomIntakeMotor;
     
-public IntakeSubsystem() {
-    topIntakeMotor = new WPI_TalonSRX(TOP_INTAKE_MOTOR_ID);
-    bottomIntakeMotor = new WPI_TalonSRX(BOTTOM_INTAKE_MOTOR_ID);
+    public IntakeSubsystem() {
+        topIntakeMotor = new WPI_TalonSRX(TOP_INTAKE_MOTOR_ID);
+        bottomIntakeMotor = new WPI_TalonSRX(BOTTOM_INTAKE_MOTOR_ID);
 
-    topIntakeMotor.setNeutralMode(NeutralMode.Brake);
-    bottomIntakeMotor.setNeutralMode(NeutralMode.Brake);
+        topIntakeMotor.setNeutralMode(NeutralMode.Brake);
+        bottomIntakeMotor.setNeutralMode(NeutralMode.Brake);
 
-    topIntakeMotor.setInverted(true);
-    bottomIntakeMotor.setInverted(false);
+        topIntakeMotor.setInverted(true);
+        bottomIntakeMotor.setInverted(false);
     }
 
-//set intake motor power
-public void topIntakeMotorPower() {
-    topIntakeMotor.set(ControlMode.PercentOutput, TOP_INTAKE_MOTOR_POWER);
-}
-public void bottomIntakeMotorPower() {
-    bottomIntakeMotor.set(ControlMode.PercentOutput, BOTTOM_INTAKE_MOTOR_POWER);
-}
+    //set intake motor power
+    public void topIntakeMotorPower() {
+        topIntakeMotor.set(ControlMode.PercentOutput, TOP_INTAKE_MOTOR_POWER);
+    }
+    public void bottomIntakeMotorPower() {
+        bottomIntakeMotor.set(ControlMode.PercentOutput, BOTTOM_INTAKE_MOTOR_POWER);
+    }
 
-//activate or deactivate the rollers
-public void setTopIntakeRoller (double power) {
-    topIntakeMotor.set(ControlMode.PercentOutput, power);
-}
+    //activate or deactivate the rollers
+    public void setTopIntakeRoller (double power) {
+        topIntakeMotor.set(ControlMode.PercentOutput, power);
+    }
 
-public void setTopIntakeRoller (boolean on) {
-    setTopIntakeRoller(on ? TOP_INTAKE_MOTOR_POWER : 0);
-}
+    public void setTopIntakeRoller (boolean on) {
+        setTopIntakeRoller(on ? TOP_INTAKE_MOTOR_POWER : 0);
+    }
 
-public void setBottomIntakeRoller (double power) {
-    bottomIntakeMotor.set(ControlMode.PercentOutput, power);
-}
+    public void setBottomIntakeRoller (double power) {
+        bottomIntakeMotor.set(ControlMode.PercentOutput, power);
+    }
 
     public void setBottomIntakeRoller (boolean on) {
         setBottomIntakeRoller(on ? BOTTOM_INTAKE_MOTOR_POWER : 0);
