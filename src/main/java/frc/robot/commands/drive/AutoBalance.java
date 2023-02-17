cc5a7705cb910e7eebd7098ea3f58d978d45d63a
RoboChicken46 <aidan.mirand@servitehs.org>
2023-02-16 21:20:42 -0800

    Autobalance iteration

--- src/main/java/frc/robot/commands/drive/AutoBalance.java
+++ src/main/java/frc/robot/commands/drive/AutoBalance.java
@@ -4,9 +4,11 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.IMU;
+import frc.robot.Robot;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.Constants;
 import java.lang.Math;
+
 
 public class AutoBalance extends CommandBase {
     
@@ -22,40 +24,52 @@ public class AutoBalance extends CommandBase {
         addRequirements(drive);
     }
 
+    public double GetError() {
+        double currentAngle = IMU.getRobotRoll().getDegrees(); //TODO Change this to pitch when IMU is oriented correctly
+
+        return Constants.Drive.CHARGE_STATION_GOAL_DEGREES - currentAngle;  
+    }
+    
     @Override
-    public void initialize() {}
+    public void initialize() {
+        errorUntilFlat = GetError();
+    }
 
     @Override
     public void execute() {
-        this.currentAngle = IMU.getRobotPitch().getDegrees();
-
-        errorUntilFlat = Constants.Drive.CHARGE_STATION_GOAL_DEGREES - currentAngle;
+        int samples = 15; //smaller means average moves faster
+        double error = GetError();
+        errorUntilFlat = (errorUntilFlat * (samples - 1) + error) / samples;
         robotSpeed = -Math.min(Constants.Drive.CHARGE_STATION_DRIVE_KP * errorUntilFlat, 1);
 
-        // for when the robot needs to drive in reverse
-        if (robotSpeed < 0) {
-        robotSpeed *= 1.35;
-        } 
+        double rawSpeed = robotSpeed;
+
+        // // for when the robot needs to drive in reverse
+        // if (robotSpeed < 0) {
+        // robotSpeed *= 1.35;
+        // } 
 
         //Limit the max speed of the robot
-        if (Math.abs(robotSpeed) > 0.4) {
-        robotSpeed = Math.copySign(0.4, robotSpeed);
+        if (Math.abs(robotSpeed) > 1) {
+        robotSpeed = Math.copySign(1, robotSpeed);
         }
 
         drive.setChassisSpeeds(new ChassisSpeeds(robotSpeed, 0, 0));   
-        
-        System.out.println("Auto balancing speed: " + robotSpeed);
-        System.out.println("Error: " + errorUntilFlat);
-        System.out.println("Current angle " + currentAngle);
+        String output = String.format("Speed: %.2f %.2f \t Error: %.2f %.2f \t Angle: %.2f", rawSpeed, robotSpeed, error, errorUntilFlat, currentAngle);
+        System.out.println(output);
     }
 
     @Override
     public void end(boolean interrupted) {
         drive.stopChassis();
+        System.out.println("Finished");
     }
 
     @Override
     public boolean isFinished() {
+        if (Robot.GameMode == Robot.EGameMode.Teleop) {
+            return false;
+        }
         return Math.abs(errorUntilFlat) < Constants.Drive.CHARGE_STATION_ANGLE_TRESHOLD_DEGREES;
     }
 }
