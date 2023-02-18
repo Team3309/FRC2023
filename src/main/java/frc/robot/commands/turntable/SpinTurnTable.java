a0f1fc964481337797c2524c6bf0cd6760f90394
RoboChicken46 <aidan.mirand@servitehs.org>
2023-02-18 12:28:19 -0800

    SpinTurnTable iteration

--- src/main/java/frc/robot/commands/turntable/SpinTurnTable.java
+++ src/main/java/frc/robot/commands/turntable/SpinTurnTable.java
@@ -1,8 +1,26 @@
 package frc.robot.commands.turntable;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.Constants.Turntable;
+import frc.robot.subsystems.TurntableSubsystem;
 
 public class SpinTurnTable extends CommandBase {
-    
-    
+    private TurntableSubsystem turntable;
+
+    public SpinTurnTable(TurntableSubsystem turntable) {
+        this.turntable = turntable;
+
+        addRequirements(turntable);
+    }
+
+    @Override
+    public void initialize() {
+        turntable.startTurntable();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        turntable.stopTurntable();
+    }
 }
