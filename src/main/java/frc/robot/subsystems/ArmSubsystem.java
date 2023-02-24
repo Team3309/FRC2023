a8d6e5e8f75e14fdda371791278f4640de12334d
RoboChicken46 <aidan.mirand@servitehs.org>
2023-02-23 21:46:13 -0800

    Untested logic for stowing the lower arm when moving the upper arm

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -29,6 +29,7 @@ public class ArmSubsystem extends SubsystemBase {
 
     public enum ArmDirection {
         Forward,
+        Inside,
         Backwards,
     }
     
@@ -58,6 +59,9 @@ public class ArmSubsystem extends SubsystemBase {
             return Backward;
         }
     }
+
+    private static double StowAngleFront = 10;
+    private static double StowAngleBack = 1000;
 
     // creating a My HashTable Dictionary
     Hashtable<ArmPosition, ArmPosePair> Poses = new Hashtable<ArmPosition, ArmPosePair>();
@@ -149,28 +153,92 @@ public class ArmSubsystem extends SubsystemBase {
     public void setPosition(ArmPosition position) {
         DesiredPosition = position;
     }
-    
+
+
+    /**
+     * Calculates if moving to a pose will cause the upper arm to pass through the robot, thus requiring the lower arm to be stowed.
+     */
+    private boolean DoesPoseRequireStowingLowerArm(ArmPose targetPose)
+    {
+        double currentPosition = UpperMotor.getActiveTrajectoryPosition();
+        double targetPosition = targetPose.UpperArm;
+
+        ArmDirection currentDirection;
+        ArmDirection targetDirection;
+
+        // -- Figure out the direction for the current position
+        if (currentPosition < StowAngleFront)
+            currentDirection = ArmDirection.Forward;
+        else if (currentPosition > StowAngleBack)
+            currentDirection = ArmDirection.Backwards;
+        else
+            currentDirection = ArmDirection.Inside;
+
+
+        // -- Figure out the direction for the target position
+        if (targetPosition < StowAngleFront)
+            targetDirection = ArmDirection.Forward;
+        else if (targetPosition >  StowAngleBack)
+            targetDirection = ArmDirection.Backwards;
+        else
+            targetDirection = ArmDirection.Inside;
+
+        if (targetDirection == ArmDirection.Inside)
+        {
+            return true;
+        }
+
+        return targetDirection != currentDirection;
+    }
+
+    private ArmPose GetCurrentPose()
+    {
+        return Poses.get(CurrentPosition).getPose(CurrentDirection);
+    }
+
+    private ArmPose GetDesiredPose()
+    {
+        return Poses.get(DesiredPosition).getPose(DesiredDirection);
+    }
+
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
         if (CurrentPosition == DesiredPosition && CurrentDirection == DesiredDirection) {
             return;
         }
-        
-        ArmPosePair posePair =  Poses.get(DesiredPosition);
-        ArmPose pose = posePair.getPose(DesiredDirection);
+
+        ArmPose pose = GetDesiredPose();
         
         if (UpperMotor.getActiveTrajectoryPosition() == pose.UpperArm && LowerMotor.getActiveTrajectoryPosition() == pose.LowerArm) {
             CurrentPosition = DesiredPosition;
             CurrentDirection = DesiredDirection;
             return;
         }
-        //TODO: make it so that the lower arm does not move untill the upper amr is passed a cerain degree 
-        //TODO: vice versa for the upper arm (need solution for deadlock)
+
+        boolean stow = DoesPoseRequireStowingLowerArm(pose);
+        if (stow)
+        {
+            ArmPose stowPose = Poses.get(ArmPosition.Stowed).getPose(ArmDirection.Forward);
+            LowerMotor.set(ControlMode.Position, stowPose.LowerArm);
+            if (LowerMotor.getActiveTrajectoryPosition() != stowPose.LowerArm)
+            {
+                return;
+            }
+        }
 
         UpperMotor.set(ControlMode.Position, pose.UpperArm);
-        // LowerMotor.set(ControlMode.Position, pose.LowerArm); //keep this disabled until the upperarm is tuned 
-        
+
+        // TODO: This code is fragile and we should think of a better way to do this
+        if (!stow) {
+            // LowerMotor.set(ControlMode.Position, pose.LowerArm); //keep this disabled until the upperarm is tuned
+        }
+        else {
+            if (!DoesPoseRequireStowingLowerArm(pose))
+            {
+                // LowerMotor.set(ControlMode.Position, pose.LowerArm); //keep this disabled until the upperarm is tuned
+            }
+        }
     }
     
     @Override
