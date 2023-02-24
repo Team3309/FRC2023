2353b55870a547c6dcd34db3f4a2fa24b6807aea
Josh Leyshock <jleyshock@kintsugiyama.com>
2023-02-24 00:46:43 -0800

    converted the arm subsystem to use commands instead of periodic

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -4,19 +4,22 @@
 
 package frc.robot.subsystems;
 
-import java.util.Hashtable;
-
-import javax.swing.text.Position;
-
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Constants.Arm;
 
-public class ArmSubsystem extends SubsystemBase {
+import java.util.Map;
 
+public class ArmSubsystem extends SubsystemBase
+{
+    // -------------------------------------------------------------------------------------------------------------------------------------
+    // -- Types
+    // -------------------------------------------------------------------------------------------------------------------------------------
     public enum ArmPosition {
         Stowed,
         PickupFloor,
@@ -29,11 +32,10 @@ public class ArmSubsystem extends SubsystemBase {
 
     public enum ArmDirection {
         Forward,
-        Inside,
         Backwards,
     }
     
-    private class ArmPose {
+    private static class ArmPose {
         public double UpperArm;
         public double LowerArm;
 
@@ -41,11 +43,11 @@ public class ArmSubsystem extends SubsystemBase {
             UpperArm = upperArm;
             LowerArm = lowerArm;
         }
-    } 
+    }
 
-    private class ArmPosePair {
-        private ArmPose Forward;
-        private ArmPose Backward;
+    private static class ArmPosePair {
+        private final ArmPose Forward;
+        private final ArmPose Backward;
 
         public ArmPosePair(ArmPose forward, ArmPose backward) {
             Forward = forward;
@@ -60,19 +62,74 @@ public class ArmSubsystem extends SubsystemBase {
         }
     }
 
-    private static double StowAngleFront = 10;
-    private static double StowAngleBack = 1000;
-
-    // creating a My HashTable Dictionary
-    Hashtable<ArmPosition, ArmPosePair> Poses = new Hashtable<ArmPosition, ArmPosePair>();
+    
+    // -------------------------------------------------------------------------------------------------------------------------------------
+    // -- Static Data
+    // -------------------------------------------------------------------------------------------------------------------------------------
+    private static final double StowAngleFront = 10;
+    private static final double StowAngleBack = 1000;
+    
+    private static final Map<ArmPosition, ArmPosePair> Poses = Map.ofEntries(
+        
+        // -- Stowed Arm
+        Map.entry(ArmPosition.Stowed,
+            new ArmPosePair(
+                new ArmPose(0, 0),
+                new ArmPose(0, 0)
+        )),
+        
+        // -- Pick up form floor
+        Map.entry(ArmPosition.PickupFloor,
+            new ArmPosePair(
+                new ArmPose(0, 0), //Forward
+                new ArmPose(0, 0)  //Backward
+        )),
+        
+        // -- Pick up from Substation
+        Map.entry(ArmPosition.PickupSubstation,
+            new ArmPosePair(
+                new ArmPose(0, 0), //Forward
+                new ArmPose(0, 0)  //Backward
+        )),
+        
+        // -- Pick up from Turntable
+        Map.entry(ArmPosition.PickupTurntable,
+            new ArmPosePair(
+                new ArmPose(0, 0), //Forward
+                new ArmPose(0, 0)  //Backward
+        )),
+        
+        // -- Score hybrid
+        Map.entry(ArmPosition.ScoreHybrid,
+            new ArmPosePair(
+                new ArmPose(0, 0), //Forward
+                new ArmPose(0, 0)  //Backward
+        )),
+        
+        // -- Score mid
+        Map.entry(ArmPosition.ScoreMid,
+            new ArmPosePair(
+                new ArmPose(0, 0), //Forward
+                new ArmPose(0, 0)  //Backward
+        )),
+        
+        // -- Score top
+        Map.entry(ArmPosition.ScoreTop,
+            new ArmPosePair(
+                new ArmPose(0, 0), //Forward
+                new ArmPose(0, 0)  //Backward
+        ))
+    );
+    
+    
+    // -------------------------------------------------------------------------------------------------------------------------------------
+    // -- Arm Subsystem
+    // -------------------------------------------------------------------------------------------------------------------------------------
 
     private final WPI_TalonFX UpperMotor;
     private final WPI_TalonFX LowerMotor;
 
-    private ArmPosition CurrentPosition;
     private ArmPosition DesiredPosition;
-
-    private ArmDirection CurrentDirection;
     private ArmDirection DesiredDirection;
 
     public ArmSubsystem() {
@@ -81,77 +138,6 @@ public class ArmSubsystem extends SubsystemBase {
         
         Constants.Arm.MOTOR_A_PID_GAINS.configureMotorPID(UpperMotor);
         Constants.Arm.MOTOR_B_PID_GAINS.configureMotorPID(LowerMotor);
-
-        // -- Stowed Arm
-        Poses.put(ArmPosition.Stowed,
-            new ArmPosePair(
-                new ArmPose(0, 0),
-                new ArmPose(0, 0)
-            )
-        );
-
-        // -- Pick up form floor
-        Poses.put(ArmPosition.PickupFloor,
-            new ArmPosePair(
-                new ArmPose(0, 0), //Forward
-                new ArmPose(0, 0)  //Backward
-        ));
-
-        // -- Pick up from Substation
-        Poses.put(ArmPosition.PickupSubstation,
-            new ArmPosePair(
-                new ArmPose(0, 0), //Forward
-                new ArmPose(0, 0)  //Backward
-        ));
-
-        // -- Pick up from Turntable
-        Poses.put(ArmPosition.PickupTurntable,
-            new ArmPosePair(
-                new ArmPose(0, 0), //Forward
-                new ArmPose(0, 0)  //Backward
-        ));
-
-        // -- Score hybrid
-        Poses.put(ArmPosition.ScoreHybrid,
-            new ArmPosePair(
-                new ArmPose(0, 0), //Forward
-                new ArmPose(0, 0)  //Backward
-        ));
-
-        // -- Score mid
-        Poses.put(ArmPosition.ScoreMid,
-            new ArmPosePair(
-                new ArmPose(0, 0), //Forward
-                new ArmPose(0, 0)  //Backward
-        ));
-
-        // -- Score top
-        Poses.put(ArmPosition.ScoreTop,
-            new ArmPosePair(
-                new ArmPose(0, 0), //Forward
-                new ArmPose(0, 0)  //Backward
-        ));
-
-    }
-
-    public boolean IsMoving() {
-        return DesiredPosition != CurrentPosition;
-    }
-   
-    public ArmPosition getCurrentPosition() {
-        return CurrentPosition;
-    }
-
-    public ArmPosition getDesiredPosition() {
-        return DesiredPosition;
-    }
-
-    public void setDirection(ArmDirection direction) {
-        DesiredDirection = direction;
-    }
-
-    public void setPosition(ArmPosition position) {
-        DesiredPosition = position;
     }
 
 
@@ -172,77 +158,73 @@ public class ArmSubsystem extends SubsystemBase {
         else if (currentPosition > StowAngleBack)
             currentDirection = ArmDirection.Backwards;
         else
-            currentDirection = ArmDirection.Inside;
-
-
+            return true;
+        
         // -- Figure out the direction for the target position
         if (targetPosition < StowAngleFront)
             targetDirection = ArmDirection.Forward;
         else if (targetPosition >  StowAngleBack)
             targetDirection = ArmDirection.Backwards;
         else
-            targetDirection = ArmDirection.Inside;
-
-        if (targetDirection == ArmDirection.Inside)
-        {
             return true;
-        }
 
         return targetDirection != currentDirection;
     }
 
-    private ArmPose GetCurrentPose()
+    private ArmPose GetPose(ArmPosition position,  ArmDirection direction)
     {
-        return Poses.get(CurrentPosition).getPose(CurrentDirection);
+        return Poses.get(position).getPose(direction);
     }
 
-    private ArmPose GetDesiredPose()
+
+    // -------------------------------------------------------------------------------------------------------------------------------------
+    // -- Commands
+    // -------------------------------------------------------------------------------------------------------------------------------------
+    public Command SetDirectionCommand(ArmDirection direction)
     {
-        return Poses.get(DesiredPosition).getPose(DesiredDirection);
-    }
-
-    @Override
-    public void periodic() {
-        // This method will be called once per scheduler run
-        if (CurrentPosition == DesiredPosition && CurrentDirection == DesiredDirection) {
-            return;
-        }
-
-        ArmPose pose = GetDesiredPose();
-        
-        if (UpperMotor.getActiveTrajectoryPosition() == pose.UpperArm && LowerMotor.getActiveTrajectoryPosition() == pose.LowerArm) {
-            CurrentPosition = DesiredPosition;
-            CurrentDirection = DesiredDirection;
-            return;
-        }
-
-        boolean stow = DoesPoseRequireStowingLowerArm(pose);
-        if (stow)
-        {
-            ArmPose stowPose = Poses.get(ArmPosition.Stowed).getPose(ArmDirection.Forward);
-            LowerMotor.set(ControlMode.Position, stowPose.LowerArm);
-            if (LowerMotor.getActiveTrajectoryPosition() != stowPose.LowerArm)
-            {
-                return;
-            }
-        }
-
-        UpperMotor.set(ControlMode.Position, pose.UpperArm);
-
-        // TODO: This code is fragile and we should think of a better way to do this
-        if (!stow) {
-            // LowerMotor.set(ControlMode.Position, pose.LowerArm); //keep this disabled until the upperarm is tuned
-        }
-        else {
-            if (!DoesPoseRequireStowingLowerArm(pose))
-            {
-                // LowerMotor.set(ControlMode.Position, pose.LowerArm); //keep this disabled until the upperarm is tuned
-            }
-        }
+        return SetPositionAndDirectionCommand(DesiredPosition, direction);
     }
     
-    @Override
-    public void simulationPeriodic() {
-        // This method will be called once per scheduler run during simulation
+    public Command SetPositionCommand(ArmPosition position)
+    {
+        return SetPositionAndDirectionCommand(position, DesiredDirection);
+    }
+
+    public Command SetPositionAndDirectionCommand(ArmPosition position,  ArmDirection direction)
+    {
+        ArmPose pose = GetPose(position, direction);
+        
+        Command MoveUpperArm = MoveJointToTargetCommand(UpperMotor, pose.UpperArm);
+        Command MoveLowerArm = MoveJointToTargetCommand(LowerMotor, pose.LowerArm);
+        
+        Command stowSequence = 
+                 StowLowerArmCommand()
+                .andThen(MoveUpperArm)
+                .andThen(MoveLowerArm);
+        
+        Command parallelMove = Commands.parallel(MoveUpperArm, MoveLowerArm);
+        
+        return new ConditionalCommand(stowSequence, parallelMove, () -> DoesPoseRequireStowingLowerArm(pose))
+                .beforeStarting(() -> {
+                    DesiredPosition = position;
+                    DesiredDirection = direction;
+                });
+    }
+
+
+    // -------------------------------------------------------------------------------------------------------------------------------------
+    // -- Internal Commands
+    // -------------------------------------------------------------------------------------------------------------------------------------
+    private Command MoveJointToTargetCommand(WPI_TalonFX motor, double position)
+    {
+        return
+             run(() -> motor.set(ControlMode.Position, position))
+            .until(() -> Math.abs(motor.getActiveTrajectoryPosition() - position) < Arm.TargetThreshold);
+    }
+    
+    private Command StowLowerArmCommand()
+    {
+        ArmPose pose = GetPose(ArmPosition.Stowed, ArmDirection.Forward);
+        return MoveJointToTargetCommand(LowerMotor, pose.LowerArm);
     }
 }
