32bb645a9008fbe3462f7ee0885810a311b97866
Josh Leyshock <jleyshock@kintsugiyama.com>
2023-02-25 01:03:46 -0800

    Removed robot.GameMode as that data was already available from RobotState.isXXX() Cleaned up some code

--- src/main/java/frc/robot/commands/drive/AutoBalance.java
+++ src/main/java/frc/robot/commands/drive/AutoBalance.java
@@ -1,42 +1,44 @@
 package frc.robot.commands.drive;
 
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.RobotState;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.IMU;
-import frc.robot.Robot;
-import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.Constants;
-import java.lang.Math;
+import frc.robot.IMU;
+import frc.robot.subsystems.DriveSubsystem;
 
 
-public class AutoBalance extends CommandBase {
-    
+public class AutoBalance extends CommandBase
+{
+
     private DriveSubsystem drive;
-    
+
     private double errorUntilFlat; //error untill the robot is flat
     private double currentAngle;
     private double robotSpeed;
 
 
-    public AutoBalance(DriveSubsystem drive) {
+    public AutoBalance(DriveSubsystem drive)
+    {
         this.drive = drive;
         addRequirements(drive);
     }
 
-    public double GetError() {
+    public double GetError()
+    {
         double currentAngle = IMU.getRobotRoll().getDegrees(); //TODO Change this to pitch when IMU is oriented correctly
-
-        return Constants.Drive.CHARGE_STATION_GOAL_DEGREES - currentAngle;  
+        return Constants.Drive.CHARGE_STATION_GOAL_DEGREES - currentAngle;
     }
-    
+
     @Override
-    public void initialize() {
+    public void initialize()
+    {
         errorUntilFlat = GetError();
     }
 
     @Override
-    public void execute() {
+    public void execute()
+    {
         int samples = 15; //smaller means average moves faster
         double error = GetError();
         errorUntilFlat = (errorUntilFlat * (samples - 1) + error) / samples;
@@ -50,24 +52,33 @@ public class AutoBalance extends CommandBase {
         // } 
 
         //Limit the max speed of the robot
-        if (Math.abs(robotSpeed) > .5) {
-        robotSpeed = Math.copySign(.5, robotSpeed);
+        if (Math.abs(robotSpeed) > .5)
+        {
+            robotSpeed = Math.copySign(.5, robotSpeed);
         }
 
-        drive.setChassisSpeeds(new ChassisSpeeds(robotSpeed, 0, 0));   
-        String output = String.format("Speed: %.2f %.2f \t Error: %.2f %.2f \t Angle: %.2f", rawSpeed, robotSpeed, error, errorUntilFlat, currentAngle);
+        drive.setChassisSpeeds(new ChassisSpeeds(robotSpeed, 0, 0));
+        String output = String.format("Speed: %.2f %.2f \t Error: %.2f %.2f \t Angle: %.2f",
+                                      rawSpeed,
+                                      robotSpeed,
+                                      error,
+                                      errorUntilFlat,
+                                      currentAngle);
         System.out.println(output);
     }
 
     @Override
-    public void end(boolean interrupted) {
+    public void end(boolean interrupted)
+    {
         drive.stopChassis();
         System.out.println("Finished");
     }
 
     @Override
-    public boolean isFinished() {
-        if (Robot.GameMode == Robot.EGameMode.Teleop) {
+    public boolean isFinished()
+    {
+        if (RobotState.isTeleop())
+        {
             return false;
         }
         return Math.abs(errorUntilFlat) < Constants.Drive.CHARGE_STATION_ANGLE_TRESHOLD_DEGREES;
