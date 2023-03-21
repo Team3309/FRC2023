daebaf8b9acc23e3c37e46a05ea231b96cce562d
RoboChicken46 <aidan.mirand@servitehs.org>
2023-03-20 19:31:56 -0700

    Test code that should add the usb camera as a working driver camera

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import friarLib2.vision.DriverCamera;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -19,6 +20,7 @@ public class Robot extends TimedRobot
 {
     private Command m_autonomousCommand;
     private RobotContainer m_robotContainer;
+    private DriverCamera m_driverCamera;
 
 
     @Override
@@ -27,6 +29,8 @@ public class Robot extends TimedRobot
         // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
         // autonomous chooser on the dashboard.
         m_robotContainer = new RobotContainer();
+
+        m_driverCamera = new DriverCamera();
     }
 
     /**
