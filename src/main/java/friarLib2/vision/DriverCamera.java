43abf09821faf944199ff67277e6e6945d91e5c0
RoboChicken46 <aidan.mirand@servitehs.org>
2023-03-20 17:43:09 -0700

    Removed double slew rate limiter Changed bindings Swerve Calibration to dashboard

--- src/main/java/friarLib2/vision/DriverCamera.java
+++ src/main/java/friarLib2/vision/DriverCamera.java
@@ -1,46 +1,27 @@
-package friarLib2.vision;
+//package friarLib2.vision;
 
-import edu.wpi.first.networktables.NetworkTableInstance;
-
-public class DriverCamera implements IVisionCamera {
-
-    @Override
-    public boolean hasTargets () {
-        return NetworkTableInstance.getDefault().getTable("Limelight").getEntry("pipeline").getDouble(0)==2;
-    }
-
-    @Override
-    public VisionTarget[] getTargets() {
-        return null;
-    }
-
-    @Override
-    public VisionTarget getBestTarget() {
-        return getTargets()[0];
-    }
-
-    @Override
-    public int getPipeline() {
-        return NetworkTableInstance.getDefault().getTable("Limelight").getEntry("pipeline").getNumber(-1).intValue();
-    }
-
-    @Override
-    public void setPipeline(int index) {
-        NetworkTableInstance.getDefault().getTable("Limelight").getEntry("pipeline").setNumber(index);
-    }
-
-    @Override
-    public void setLights(LedMode mode) {
-        switch (mode) {
-            case off:
-
-        }
-    }
-
-    @Override
-    public int camMode() {
-        return (int) NetworkTableInstance.getDefault().getTable("Limelight").getEntry("pipeline").getNumber(1);
-    }
-
-}
-
+//import edu.wpi.first.networktables.NetworkTableInstance;
+//
+//public class DriverCamera implements IVisionCamera {
+//
+//    @Override
+//    public boolean hasTargets() {
+//    return NetworkTableInstance.getDefault().getTable("camera").getEntry("pipeline").getDouble(2) == 1;
+//    }
+//
+//    @Override
+//    public VisionTarget[] getTargets() {
+//        return null;
+//    }
+//
+//    @Override
+//    public VisionTarget getBestTarget() {
+//        return getTargets()[0];
+//    }
+//
+//    @Override
+//    public int getPipeline() {
+//        return NetworkTableInstance.getDefault().getTable("camera").getEntry("+")
+//    }
+//
+//}
