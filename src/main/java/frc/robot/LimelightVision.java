b6baee578317d19870c033b287466246a062e3f6
Robochicken46 <aidanmirand123@gmail.com>
2023-12-30 14:57:23 -0800

    Distance Estimator

--- src/main/java/frc/robot/LimelightVision.java
+++ src/main/java/frc/robot/LimelightVision.java
@@ -1,6 +1,9 @@
 package frc.robot;
 
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import friarLib2.vision.LimelightCamera;
@@ -28,29 +31,28 @@ public class LimelightVision {
     public static IVisionCamera shooterCamera = new LimelightCamera();
 
     private static final PixelToAngle ANGLE_CONVERTER = new PixelToAngle(320, 240, 54, 41); // Constants for the limelight 2
-    private static final double HEIGHT_OF_CAMERA = 0.7747; // Meters
-    private static final double ANGLE_OF_CAMERA = 34.8; // Degrees
+    private static final double HEIGHT_OF_CAMERA = 49.61; // inches
+    private static final double ANGLE_OF_CAMERA = 160; // Degrees
 
     private static double lastDistance = 0; // Return this if the robot does not have a target
 
     /**
      * @return the distance in meters from the target
      */
-    public static double getMetersFromTarget () {
+    public static double getInchesFromTarget () {
         try {
-            double a2;
-            if (shooterCamera instanceof PhotonCameraWrapper) {
-                a2 = shooterCamera.getBestTarget().getY();
-            } else {
-                a2 = ANGLE_CONVERTER.calculateYAngle(shooterCamera.getBestTarget());
-            }
+            NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+            NetworkTableEntry ty = table.getEntry("ty");
+            double a2 = ty.getDouble(0.0);
             double a1 = ANGLE_OF_CAMERA;
             double h1 = HEIGHT_OF_CAMERA;
-            double h2 = Units.inchesToMeters(12*8 + 8); // Height of target
+            double h2 = 26; // Place holder Height of target
 
-            lastDistance = (h2 - h1) / Math.tan(Math.toRadians(a1 + a2));
-        } catch (IndexOutOfBoundsException e) {} // If the camera has no target
+            double angleToGoal = (a1 + a2);
+            double angleToGoalRadian = Math.toRadians(angleToGoal);
+            lastDistance = (h2-h1) / Math.tan(angleToGoalRadian);
 
+        } catch (IndexOutOfBoundsException e) {} // If the camera has no target the target has no 
         return lastDistance;
     }
 }
